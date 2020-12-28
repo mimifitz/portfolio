3446567008acb32ce971d395c0167f61333ce46f
Miss Ajo <Mimi Fitz>
2020-12-28 01:45:46 +0000

    Programm smooth scroll and flow of the page

--- pages/main.js
+++ pages/main.js
@@ -1,9 +1,36 @@
+/*Appearancing of hamburger menu by click on the icon*/
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
 
     });
-});
 
-/*Appearancing of hamburger menu by click on the icon*/
+
+    /*hamburger*/
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+
+    /*Smooth scrolling*/
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate(keyframes: {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options: 2000);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate(keyframes: {
+            scrollTop: 0
+        }, options: 2000);
+    });
+
+    /*Initializing animation efect*/
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
+});
