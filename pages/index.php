fb889a500545e7766dc27b2e21d6996b40f73a13
Miss Ajo <Mimi Fitz>
2021-01-21 14:23:44 +0000

    refactored

--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,33 @@
-<link rel="stylesheet" href="style.css">
-<f<link rel="stylesheet" href="style.css">orm action="contact.php" method="post">
+<?php
+if (isset($_POST['submit']))
+{
+$name = $_POST['name'];
+$email = $_POST['email'];
+$services = $POST['services'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+ $formcontent="From: $name \n Message: $message";
+ $recipient = "missajo358@gmail.com";
+ $subject = "Job Position Enquiries";
+ $mailheader = "From: $email \r\n";
+ mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+ echo "Thank You, Your message has been sent!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff00b3;'> Return Home</a>";
+ ?>
+ }
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" type="text">
+</head>
+<body>
+ <section class="contact" id="contact">
+        <section div class="container">
+            <div class="section-heading">
+                <h1>Contact</h1>
+                <h6><b>Let's Work Together & Impact Positively</b></h6>
+                <form action="contact.php" method="post">
                     <label for="name">Name or Company Name:</label>
                     <input type="text" id="name" name="name" placeholder="Please enter your name or company's name..."
                         pattern=[A-Z\sa-z]{3,20} required>
@@ -19,4 +47,8 @@
                     <textarea name="message" id="message" cols="10" rows="10"></textarea>
                     <input type="submit" name="submit" value="Submit" id="submit" data-submit="...Sending"
                         aria-required="true">
-                </f>
+                </form>
+            </div>
+        </section>
+</body>
+</html>
