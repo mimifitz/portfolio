906c92d2b7f4f1965a223494b8f24a8e286b467e
Miss Ajo <Mimi Fitz>
2021-01-27 23:23:06 +0000

    COMNTACT.PHP refactored and updated

--- pages/contact.php
+++ pages/contact.php
@@ -3,46 +3,22 @@ if (isset($_POST['submit']))
 {
 $name = $_POST['name'];
 $email = $_POST['email'];
+$contact = $_POST['contact'];
 $services = $POST['services'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
- $formcontent="From: $name \n Message: $message";
- $recipient = "missajo358@gmail.com";
+ $to='missajo358@gmail.com';
+ $formcontent="From:".$name."\n"."Contact:".$contact."\n"." Message: $message";
  $subject = "Job Position Enquiries";
- $mailheader = "From: $email \r\n";
- mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
- echo "Thank You, Your message has been sent!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff00b3;'> Return Home</a>";
+ $mailheaders = "From:".$email;
+
+  if(mail($to, $formcontent, $subject, $mailheaders)){
+ echo "Thank You, Message Sent Successfully!".".$name.", "<a href='index.html' style='text-decoration:none;color:#ff00b3;'> Return Home</a>";
+}
+else{
+	echo "OOPPSS! Something went wrong"
+}
 }
  ?>
-
-
-<!-- $to = $email;
-    $subject = $subjectName;
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: '.$email.'<'.$email.'>' . "\r\n".'Reply-To: '.$mail."\r\n" . 'X-Mailer: PHP/' . phpversion();
-    $message = '<!doctype html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport"
-					  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Document</title>
-			</head>
-			<body>
-			<span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">'.$message.'</span>
-				<div class="container">
-                 '.$message.'<br/>
-                    Regards<br/>
-                  '.$email.'
-				</div>
-			</body>
-			</html>';
-    $result = @mail($to, $subject, $message, $headers);
-
-    echo '<script>alert("Email sent successfully !")</script>';
-    echo '<script>window.location.href="index.php";</script>'; -->
-
 
